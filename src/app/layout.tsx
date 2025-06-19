24a7e014f388aa57ba3d5917a71ec4b97245ecd0
ValentinGarro <vgarro@coradir.com.ar>
2025-06-19 17:20:06 -0300

    arreglando  tag manager /arreglando google analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,13 +10,7 @@ export default function RootLayout({
     <html lang="es">
     <head>
       {/* Google Tag Manager */}
-      <script>
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-PBZQ65VZ');`}
-      </script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-K747C6S23H"></script> <script>{`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-K747C6S23H'); `}</script>
       {/* Google Analytics */}
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-0FW1131XF8"></script>
       <script>
@@ -29,8 +23,8 @@ export default function RootLayout({
     <body className="font-dm scroll-smooth">
       {/* Google Tag Manager (noscript) */}
       <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBZQ65VZ"
-          height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WPC2GBF9"
+       height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
       </noscript> 
       <Header />
       {children}
