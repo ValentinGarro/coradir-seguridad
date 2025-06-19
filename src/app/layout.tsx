a5a85a6e00b84300d4298bd5ff395b9f354d844a
ValentinGarro <vgarro@coradir.com.ar>
2025-06-19 16:38:43 -0300

    agregando tag manager / agregando google analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className="font-dm scroll-smooth">
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <head>
+      {/* Google Tag Manager */}
+      <script>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PBZQ65VZ');`}
+      </script>
+      {/* Google Analytics */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-0FW1131XF8"></script>
+      <script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-0FW1131XF8');`}
+      </script>
+    </head>
+    <body className="font-dm scroll-smooth">
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBZQ65VZ"
+          height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+      </noscript> 
+      <Header />
+      {children}
+      <Footer />
+    </body>
+  </html>
   );
 }
