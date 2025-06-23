cafda785ee7a095efa8b105ec1c6e115ee38e65c
ValentinGarro <vgarro@coradir.com.ar>
2025-06-23 08:03:43 -0300

    arreglando error de hiddratacion scrips tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,18 +8,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-    <head>
-      {/* Google Tag Manager */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-K747C6S23H"></script> <script>{`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-K747C6S23H'); `}</script>
-      {/* Google Analytics */}
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-0FW1131XF8"></script>
-      <script>
-        {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-0FW1131XF8');`}
-      </script>
-    </head>
+    <head>{/* Google Tag Manager */}<script async src="https://www.googletagmanager.com/gtag/js?id=G-K747C6S23H"></script><script>{`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-K747C6S23H'); `}</script>{/* Google Analytics */}<script async src="https://www.googletagmanager.com/gtag/js?id=G-0FW1131XF8"></script><script>{`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-0FW1131XF8');`}</script></head>
     <body className="font-dm scroll-smooth">
       {/* Google Tag Manager (noscript) */}
       <noscript>
