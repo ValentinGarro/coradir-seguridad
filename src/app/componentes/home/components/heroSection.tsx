9b079b1edb5f83571c9c094c74527b1c777372ed
ValentinGarro <vgarro@coradir.com.ar>
2025-06-23 09:53:53 -0300

    arreglando estilos a la seccion de hero p en xl

--- src/app/componentes/home/components/heroSection.tsx
+++ src/app/componentes/home/components/heroSection.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
     return (
         <section className="flex flex-col justify-start items-center bg-white">
             < div
-                className="w-full h-[50vh] md:h-[80vh] xl:h-[100vh] flex flex-col items-end max-w-[1800px] xl:justify-center justify-end overflow-hidden"
+                className="w-full h-[50vh] md:h-[80vh] xl:h-[100vh] flex flex-col items-end  max-w-[1800px] xl:justify-center justify-end overflow-hidden"
                 style={{
                     backgroundImage: "url('/img/hero.jpg')",
                     backgroundSize: "cover",
@@ -18,16 +18,16 @@ export default function HeroSection() {
             >
                 <motion.h1 
                 ref={bannerRef}
-                    className="bg-red/30 rounded-bl-3xl text-4xl xl:text-6xl font-bold pl-5   md:pl-20 py-2  w-[90%]  text-left text-white"
+                    className="bg-red/30 rounded-bl-3xl text-4xl xl:text-6xl font-bold pl-5   md:pl-20  py-2  w-[90%]  text-left text-white"
                     initial={{ opacity: 0, x: "50%" }}
                     animate={isInView ? { opacity: 1, x: "0%" } : { opacity: 0, x: "50%" }}
                     transition={{ duration: 0.8, ease: "easeOut" }}
                 >PROTECCIÓN <br className="xl:hidden"/> INTEGRAL<br className="hidden xl:block"/> PARA <br className="xl:hidden"/> COMUNIDADES <br /> MÁS SEGURAS</motion.h1>
-                <p className=" p-3 terxt-lg bg-red text-white text-center rounded-tr-2xl container rounded-bl-2xl w-[40%] md:block hidden text-xl">
+                <p className=" p-3 terxt-lg bg-red text-white text-left rounded-tr-2xl  rounded-bl-2xl w-[40%] md:block hidden text-xl xl:text-4xl xl:px-20 xl:w-[60%] xl:mr-[25%] xl:p-12">
                   Cuidamos tu tranquilidad, la de quienes te rodean y nos adaptamos a tus necesidades
                 </p>
             </div> 
-           <p className=" p-3 terxt-lg bg-red text-white text-center rounded-tr-2xl container rounded-bl-2xl w-[80%] md:w-[40%] md:hidden xl:text-xl">
+           <p className=" p-3 terxt-lg bg-red text-white text-center rounded-tr-2xl container rounded-bl-2xl w-[80%] md:w-[40%] md:hidden ">
            Cuidamos tu tranquilidad, la de quienes te rodean y nos adaptamos a tus necesidades
            </p>
         </section>
